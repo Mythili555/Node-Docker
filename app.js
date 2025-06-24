87871b9a28e8f911e9bf8b9b783b174e8f2a57ba
Mythili555 <116316394+Mythili555@users.noreply.github.com>
2025-06-24 17:08:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-	res.send('CICD Pipeline testing successfull-updated')
+	res.send('CICD Pipeline testing successfull')
 })
 
 var server = app.listen(5000, function () {
